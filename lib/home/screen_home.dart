826870c8c6f82c861ce8702af1fcd38dfc8513c2
HemanthAK <hemanthbovikkanam@gmail.com>
2023-04-16 11:45:21 +0530

    after the home page

---
+++ lib/home/screen_home.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        title: const Text('DO THERE',
+            style:TextStyle(
+              color: Colors.white54,
+              fontSize: 20,
+              fontStyle: FontStyle.italic
+            )),
+        backgroundColor: Colors.black45,
+      ),
+      body: SafeArea(
+        
+        child: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 70,
+              bottom: 25,
+            ),
+            child: Center(
+                child: Column(
+              children: [
+                Container(
+                  height: 150,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        //<-- SEE HERE
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                          image: NetworkImage(
+                              'https://png.pngtree.com/png-vector/20190419/ourlarge/pngtree-vvector-notification-icon-png-image_957092.jpg'),
+                          fit: BoxFit.contain)),
+                  // child: Image.network(
+                  //     'https://png.pngtree.com/png-vector/20190419/ourlarge/pngtree-vvector-notification-icon-png-image_957092.jpg'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Notify',
+                      style: TextStyle(color: Colors.white54, fontSize: 20)),
+                )
+              ],
+            )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 70,
+              bottom: 25,
+            ),
+            child: Center(
+                child: Column(
+              children: [
+                Container(
+                  height: 150,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        //<-- SEE HERE
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                      image:const DecorationImage(
+                          image: NetworkImage(
+                              'https://cdn-icons-png.flaticon.com/512/3799/3799832.png'),
+                          fit: BoxFit.contain)),
+                  // child: Image.network(
+                  //     'https://cdn-icons-png.flaticon.com/512/3799/3799832.png'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Reminder',
+                      style: TextStyle(color: Colors.white54, fontSize: 20)),
+                )
+              ],
+            )),
+          ),
+        ],
+      )),
+    );
+  }
+}
